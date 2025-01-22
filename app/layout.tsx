4d12eab2af7c2f3bb93a2a1036980f833b676ce9
kod-it <akash.bitd@gmail.com>
2025-01-23 00:31:29 +0530

    font update

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,9 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
+      <head>
+      <link href="https://fonts.googleapis.com/css2?family=Century+Gothic&display=swap" rel="stylesheet"/>
+      </head>
       <body className="antialiased max-w-5xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
